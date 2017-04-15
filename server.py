ce43cbdc218c35c13996cf84f45ae3e67ec6d8dd
Will <will.sherwood5@gmail.com>
2017-04-15 18:03:33 -0500

    move template folder

--- server.py
+++ server.py
@@ -1,17 +1,32 @@
 from flask import Flask
 from flask import request
+from flask import send_from_directory
+from flask import render_template
+
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'world'
+    return render_template('index.html')
 
 @app.route('/compile', methods=['POST', 'GET'])
 def compile():
     # subprocess.Popen('lilypond', shell=True, stdout=subprocess.PIPE).stdout.read()
     return 'hello miles'
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+@app.route('/fonts/<path:path>')
+def send_fonts(path):
+    return send_from_directory('fonts', path)
 
 if __name__ == '__main__':
     app.run()
