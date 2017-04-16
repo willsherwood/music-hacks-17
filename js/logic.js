bad2f73e9b7d1188a12d899eb1474cbe92a5515d
willsherwood <will.sherwood5@gmail.com>
2017-04-16 15:57:29 -0500

    begin rudiment book and other styling things

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,33 @@
-$(document).ready(function() {
-    $('#Create-btn').on('click', function(e) {
+function newMeasure() {
+    $('#input-form').append($("<div class=\"col-sm-4\"><textarea class=\"form-control input-lg\"></textarea></div>"));
+}
+
+function getValue() {
+    var out = "";
+    var children = $('#input-form').children();
+    for (var i = 0; i < children.length; i++) {
+        var measure = children[i].children[0];
+        out += measure.value + " ";
+    }
+    return out;
+}
+
+$(document).ready(function () {
+
+    newMeasure();
+
+    $('#measureMenuNewMeasure').on('click', function (e) {
+        newMeasure();
+    });
+
+    $('#compile').on('click', function (e) {
         $.ajax({
             type: "POST",
             url: "compile",
-            data: $('#comment').val(),
-            success: function(data) {
-                $("#sheets").attr('src', data);
-		$("#share").html('Share your lick: <a href="' + data + '"><i>Here</i></a>');
+            data: getValue(),
+            success: function (data) {
+                $("#lickImg").attr('src', data);
+                $("#share").html('Share your lick: <a href="' + data + '"><i>Here</i></a>');
             }
         });
     });
